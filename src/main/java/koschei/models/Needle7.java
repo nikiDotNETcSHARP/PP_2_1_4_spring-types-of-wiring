e201828c6bdb4772291c7ef1ac81860defe77094
Nikita Noskov <noskovnikita@lenta.ru>
2024-05-17 18:19:40 +0300

    fixСlassNeedle7

--- src/main/java/koschei/models/Needle7.java
+++ src/main/java/koschei/models/Needle7.java
@@ -5,15 +5,8 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Needle7 {
-    private final Deth8 deth;
-
-    @Autowired
-    public Needle7(Deth8 deth) {
-        this.deth = deth;
-    }
-
     @Override
     public String toString() {
-        return ", смерть Кощея на игле :( " + deth.toString();
+        return ", смерть Кощея на игле :( " + "";
     }
 }
